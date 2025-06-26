bb8f52c9e9ecdf608e4b6ff65f67ff399d7df434
bhagatankit05 <bhagatankit82@gmail.com>
2025-06-26 21:58:03 +0530

    passing filename from backend

--- simple_project/index.js
+++ simple_project/index.js
@@ -10,11 +10,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    fs.readdir(`./files`,function(err, files) {
-  res.render("index",{files: files});
-}   );
+    fs.readdir(`./files`, function (err, files) {
+        res.render("index", { files: files });
+    });
 })
 
+app.post('/create', (req, res) => {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details,function(err){
+        res.redirect('/');
+});
+})
+
+
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+    console.log('Server is running on http://localhost:3000');
 });
