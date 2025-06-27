1615124b285832c58a76df42c7cfabe668b1daaf
bhagatankit05 <bhagatankit82@gmail.com>
2025-06-27 15:36:08 +0530

    basic level CRUD operation

--- mongoDB/app.js
+++ mongoDB/app.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const app = express()
 
-app.get('/',(req,res)=>{
+const userModel = require('./usermodel')
+
+app.get('/', (req, res) => {
     res.send("hello World")
 })
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 30000")
+app.get('/create', async (req, res) => {
+
+    let createdUser = await userModel.create({
+        name: "Ankit Bhagat",
+        username: "ankitbhagat",
+        email: "bhagatankit82@gmail.com"
+    })
+    res.send(createdUser);
+
+})
+
+app.get('/read', async (req,res)=>{
+    let readUser  = await userModel.find();
+    res.send(readUser);
+})
+
+app.get('/update', async (req,res)=>{
+    let updatedUser = await userModel.findOneAndUpdate({username:"ankitbhagat"},{name: "Ankit R Bhagat"},{new : true})
+    res.send(updatedUser);
+
+    })
+
+app.get('/delete', async (req,res)=>{
+    let users = await userModel.findOneAndUpdate({username:"ankitbhagat"})
+    res.send(users);
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000")
 })
